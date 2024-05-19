df0af03ae4a7310cfc234d04408fc883af967e74
bartek623 <bartlomiej.kowalczuk12@gmail.com>
2024-05-19 13:04:44 +0200

    line simplification

--- src/components/canvas/LineElement.tsx
+++ src/components/canvas/LineElement.tsx
@@ -3,6 +3,7 @@ import { Vector2 } from "three";
 import { LINE_TYPE, LineDataType } from "../../utils/types";
 import { PointsToGeometry } from "./PointsToGeometry";
 import { getCurvePoints } from "./utils";
+import { Line } from "@react-three/drei";
 
 type LineElementProps = LineDataType & {
   showGeometry: boolean;
@@ -21,29 +22,17 @@ export function LineElement(props: LineElementProps) {
     points = getCurvePoints(start, end, center, counterClockwise);
   }
 
-  useLayoutEffect(() => {
-    if (!lineRef.current) return;
-
-    //@ts-expect-error there is currently no type for line ref
-    lineRef.current.geometry.setFromPoints(
-      points.map((point) => new Vector2(...point))
-    );
-    // @ts-expect-error as above
-    if (type === LINE_TYPE.POSITIONING) lineRef.current.computeLineDistances();
-  });
-
   const isDashed = type === LINE_TYPE.POSITIONING;
 
   return (
     <>
-      <line ref={lineRef}>
-        <bufferGeometry />
-        {isDashed ? (
-          <lineDashedMaterial color="blue" dashSize={0.08} gapSize={0.03} />
-        ) : (
-          <lineBasicMaterial color="red" />
-        )}
-      </line>
+      <Line
+        points={points}
+        color={isDashed ? "blue" : "red"}
+        dashed={isDashed}
+        dashSize={0.08}
+        gapSize={0.03}
+      />
       {showGeometry && !isDashed && (
         <PointsToGeometry pointsData={points.map((p) => [p.x, p.y, 0])} />
       )}
